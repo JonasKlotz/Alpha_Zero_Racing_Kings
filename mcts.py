aff805322f1d8df77f73993251bb51db1a4de682
Sebastian Zimmermann <seba@Sebastians-MacBook-Pro-3.fritz.box>
2020-05-04 15:37:16 +0200

    added recursion
    addressing #20 but still need to
    figure out how to pass game
    position down the tree.

--- mcts.py
+++ mcts.py
@@ -12,7 +12,7 @@ QMEANVALUE = 3
 CHILDNODE = 4
 
 
-class mock_translator():
+class MockTranslator():
     def get_legal_moves(self, position):
         legal_moves = np.random.rand(*DIMENSIONS)
         legal_moves[legal_moves > 0.5] = 1
@@ -20,7 +20,7 @@ class mock_translator():
         return legal_moves
 
 
-class mock_model():
+class MockModel():
     def inference(self, position):
         p = np.random.rand(*DIMENSIONS)
         v = np.random.rand(1)[0]
@@ -28,7 +28,7 @@ class mock_model():
         return (p, v)
 
 
-class mcts():
+class Mcts():
     def __init__(self, translator, model, sims_per_move=10):
         self.translator = translator
         self.modell = modell
@@ -45,35 +45,80 @@ class mcts():
 
 
 
-class node():
+class Node():
     def __init__(self, translator, model, position):
 
+        self.v = 0 
+        self.endposition = False
         legal_moves = translator.get_legal_moves(position)
         self.legal_move_indices = np.where(legal_moves == 1)
         num_of_legal_moves = len(self.legal_move_indices[0])
         self.children = [None] * num_of_legal_moves
 
-        p, self.v = model.inference(position)
-        p_legal = p * legal_moves 
+        if num_of_legal_moves == 0:
+            # reached end of game
+            self.v = translator.get_result(position)
+            self.endposition = True
 
-        # initialise tensor to hold 
-        # 5 values per edge
-        entries_per_edge = 5
-        self.edges = np.zeros((*legal_moves.shape, entries_per_edge))
+        else: 
+            # expanding leaf node
+            p, self.v = model.inference(position)
+            p_legal = p * legal_moves 
 
-        # store prior values from
-        # p_legal in edges
-        self.edges[:,:,:,PPRIOR] = p_legal 
+            # initialise tensor to hold 
+            # 5 values per edge
+            entries_per_edge = 5
+            self.edges = np.zeros((*legal_moves.shape, entries_per_edge))
 
-        # for every legal move,
-        # store list index which refers to
-        # corresponding child node
-        legal_edges = self.all_legal_moves()
-        legal_edges[:,CHILDNODE] = np.arange(num_of_legal_moves) 
+            # store prior values from
+            # p_legal in edges
+            self.edges[:,:,:,PPRIOR] = p_legal 
+
+            # for every legal move,
+            # store list index which refers to
+            # corresponding child node
+            legal_edges = self.all_legal_moves()
+            legal_edges[:,CHILDNODE] = np.arange(num_of_legal_moves) 
 
 
-    def get_evaluation(self):
-        return self.v
+    def rollout(self):
+        '''
+        recursive traversal of game tree
+        updates P, N, W, Q of all travelled
+        edges
+        '''
+        if self.endposition:
+            # terminate recursion
+            # if end state is reached
+            return self.v
+
+        i = self.index_of_best_move()
+        j = self.edges[i][CHILDNODE]
+        nextnode = self.children[j]
+        v = 0
+
+        if nextnode == None:
+            # terminate recursion
+            # on leaf expansion
+            # TODO: where do we get new position?
+            newposition = Null
+            leaf = Node(self.translator, self.model, newposition)
+            v = leaf.v
+
+        else:
+            # recursion
+            v = nextnode.rollout()
+
+        # calculate edge stats
+        count = self.edges[i][NCOUNT] + 1
+        accum = self.edges[i][WACCUMVALUE] + v 
+
+        # update edge stats
+        self.edges[i][QMEANVALUE] = accum / count
+        self.edges[i][NCOUNT] = count
+        self.edges[i][WACCUMVALUE] = accum
+
+        return v
 
 
     def index_of_best_move(self):
