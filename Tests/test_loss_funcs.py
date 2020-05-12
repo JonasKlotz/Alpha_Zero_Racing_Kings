96daf7172be0a0ec21ec0dc7bfce966347cb6796
Konstantin Ausborn <konstantin.ausborn@protonmail.com>
2020-05-12 17:04:29 +0200

    Skeleton for tests of loss functions added

---
+++ Tests/test_loss_funcs.py
@@ -0,0 +1,16 @@
+data1 = []
+labels1 = []
+data2 = []
+labels2 = []
+
+def test_mse():
+    #test if mse works as expected
+    assert True
+
+def test_crossentropy():
+    #test if ce works as expected
+    assert True
+
+def test_mse_crossentropy_added():
+    #test if our custom combination works as expected
+    assert True
