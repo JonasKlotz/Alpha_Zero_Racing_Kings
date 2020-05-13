124bad1840367af66f0f9b8ef1214beb9076c2ab
Konstantin Ausborn <konstantin.ausborn@protonmail.com>
2020-05-13 15:28:54 +0200

    Trying to adhere to pep more

--- Tests/test_loss_funcs.py
+++ Tests/test_loss_funcs.py
@@ -1,27 +1,35 @@
+"""tests for loss funcs
+"""
 import numpy as np
 from tensorflow import keras
 
 pred_1 = np.array([0.0, 1.00, 0.75, -0.1])
 labels_1 = np.array([0.1, 1.05, 0.750, 0.1])
-true_mse_1 = 0.013125
-true_ce_1 = 0
+TRUE_MSE_1 = 0.013125
+TRUE_CE_1 = 0
 
 pred_2 = []
 labels_2 = []
 
-losses = {'outcome':'mean_squared_error', 'policy':'categorical_crossentropy'}
-losses_weights = {'value':1, 'policy':1}
-#test if the return format of built-in loss functions is as simple as expected
+losses = {'outcome': 'mean_squared_error', 'policy': 'categorical_crossentropy'}
+losses_weights = {'value': 1, 'policy': 1}
+
+
 def test_mse():
+    """test if the return format of built-in loss functions is as simple as expected
+    """
     keras_mse_1 = keras.losses.mean_squared_error(pred_1, labels_1)
-    assert np.isclose(keras_mse_1, true_mse_1)
-    #now test for second dataset
+    assert np.isclose(keras_mse_1, TRUE_MSE_1)
+
 
 def test_crossentropy():
+    """test if the return format of built-in loss functions is as simple as expected
+    """
     keras_ce_1 = keras.losses.binary_crossentropy(pred_1, labels_1)
-    assert np.isclose(keras_ce_1, true_ce_1)
+    assert np.isclose(keras_ce_1, TRUE_CE_1)
 
-#custom loss function test
+
 def test_mse_crossentropy_added():
-    #test if our custom combination works as expected
+    """test if our custom combination works as expected
+    """
     assert True
