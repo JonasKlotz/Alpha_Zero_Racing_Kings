fa9ce704c014d4291c1cbe0174f53d326f160715
Jonas Klotz <jonasklotz@hotmail.de>
2020-05-13 19:10:32 +0200

    added logger.py we need to setup logger with a standart logging path then add : logger = getLogger(__name__)
    in every class we want to log

--- lib/logger.py
+++ lib/logger.py
@@ -4,8 +4,20 @@ Logging helper methods
 
 from logging import StreamHandler, basicConfig, DEBUG, getLogger, Formatter
 
+"""
+we need to setup logger with a standard logging path
+then add :
+logger = getLogger(__name__)
+
+in every class we want to log
+"""
+
 
 def setup_logger(log_filename):
+    """
+    setup a logger that logs lol
+    :arg log_filename : path to logfile
+    """
     format_str = '%(asctime)s@%(name)s %(levelname)s # %(message)s'
     basicConfig(filename=log_filename, level=DEBUG, format=format_str)
     stream_handler = StreamHandler()
