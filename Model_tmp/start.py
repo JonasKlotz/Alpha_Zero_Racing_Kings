705e57758f178a4d8be7a2385028afea2f6e526a
Kurt Willis <willis@campus.tu-berlin.de>
2020-05-13 22:07:32 +0200

    renaming model folder

--- Model_tmp/start.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from model import AZero
-
-azero = AZero('Model/config.yaml')
-azero.summary()
-
-# graphviz (not a python package) has to be installed https://www.graphviz.org/
-azero.plot_model()
