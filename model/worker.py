44a93dd6de57c38c41c94b866285b5a0ae1ddc31
Kurt Willis <willis@campus.tu-berlin.de>
2020-05-14 19:06:56 +0200

    working training runner

--- model/worker.py
+++ model/worker.py
@@ -1,9 +1,7 @@
 import os
-# import numpy as np
 import pickle
 
 from Model.model import AZero
-# from Model.mcts_temp import MCTS
 from Interpreter.game import Game
 from Model.config import Config
 
@@ -19,51 +17,33 @@ class Worker:
     def read_config(self, config_file):
         self.config = Config(config_file)
 
-    def load_train_data(self):
-        file = os.path.join(self.config.train_data_dir, 'example.tar')
+    def load_dummy_data(self):
+        file = 'Model/dummy_data.pkl'
         with open(file, 'rb') as f:
             self.train_data = pickle.load(f)
-        print("training data {} loaded".format(file))
+        print("dummy data loaded")
+
+    def load_train_data(self):
+        file = os.path.join(self.config.train_data_dir, 'batch.pkl')
+        with open(file, 'rb') as f:
+            self.train_data = pickle.load(f)
+        print("training data loaded from {}".format(file))
 
     def save_train_data(self):
-        file = os.path.join(self.config.train_data_dir, 'example.tar')
+        file = os.path.join(self.config.train_data_dir, 'batch.pkl')
         with open(file, 'wb') as f:
             pickle.dump(self.train_data, f)
-        print("training data saved in {}".format(file))
+        print("Training data saved to {}".format(file))
 
-    def run(self):
-        pass
-        # for _ in range(10):
-        #     mcts = MCTS(self.game, self.azero, 1, 100)
-        #     self.train_data += self.game_playout(mcts)
-
-    def game_playout(self, mcts):
-        pass
-        # data = []
-        # self.game.reset()
-        # while True:
-        #     temp = 1
-        #     current_player = self.game.player_to_move   # {-1,1}
-
-        #     pi = mcts.getActionProb(temp=temp)
-        #     s = self.game.get_state_fen()
-
-        #     action = np.random.choice(self.game.get_move_list_str(), p=pi)
-        #     self.game.make_move(action)
-
-        #     r = self.game.get_score(self.game.get_current_player())
-
-        #     r = int((1 - .5) * 2)   # rescale to [-1,1]
-        #     # r = r * current_player    # XXX rethink
-
-        #     data.append([s, pi, r])
-
-        #     if r != 0:
-        #         return data
+    def train(self):
+        print("Beginning training")
+        self.azero.train(self.train_data)
 
 
 if __name__ == "__main__":
     worker = Worker('Model/config.yaml')
+    worker.load_dummy_data()
+    worker.train()
 
     # worker.azero.summary()
     # worker.azero.plot_model()
