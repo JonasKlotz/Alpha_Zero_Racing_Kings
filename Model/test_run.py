64a0cc247b1b5a0b74d8e7651177208977444152
Kurt Willis <willis@campus.tu-berlin.de>
2020-05-17 21:22:50 +0200

    time wrapper

--- Model/test_run.py
+++ Model/test_run.py
@@ -5,22 +5,29 @@ from Model.model import AZero
 from Model.config import Config
 
 
-def timing(f):
-    def wrap(*args):
+def timing(func):
+    """ wrapper for timing functions
+    usage:
+    @timing
+    def time_test():
+        myfunc()
+    time_test()
+    """
+    def wrap(*args, **kwargs):
         time1 = time.time()
-        ret = f(*args)
+        ret = func(*args, **kwargs)
         t = time.time() - time1
         h = int(t / 3600)
         m = int((t % 3600) / 60)
         s = int((t % 60))
         if h is not 0:
-            print('{:s}() took {}h {}m {}s'.format(f.__name__, h, m, s))
+            print('{:s}() took {}h {}m {}s'.format(func.__name__, h, m, s))
         elif m is not 0:
-            print('{:s}() took {}m {}s'.format(f.__name__, m, s))
+            print('{:s}() took {}m {}s'.format(func.__name__, m, s))
         elif s is not 0:
-            print('{:s}() took {:.2f}s'.format(f.__name__, t))
+            print('{:s}() took {:.2f}s'.format(func.__name__, t))
         else:
-            print('{:s}() took {:.2f}ms'.format(f.__name__, t*1000))
+            print('{:s}() took {:.2f}ms'.format(func.__name__, t*1000))
         return ret
     return wrap
 
@@ -29,9 +36,9 @@ if __name__ == "__main__":
     print("===== Executing Test Run =====")
     config = Config('Model/config.yaml')
     azero = AZero(config)
-    file = '_Data/training_data/dataset_685_games.pkl'
-    #file = '_Data/training_data/game_0000.pkl'
-    with open(file, 'rb') as f:
+    FILE = '_Data/training_data/dataset_685_games.pkl'
+    #FILE = '_Data/training_data/game_0000.pkl'
+    with open(FILE, 'rb') as f:
         train_data = pickle.load(f)
 
     @timing
