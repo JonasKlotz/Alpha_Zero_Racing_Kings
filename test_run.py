306288c3f27488c12865ade0a690999db69a9e08
Kurt Willis <willis@campus.tu-berlin.de>
2020-05-17 21:34:17 +0200

    timing wrapper

--- test_run.py
+++ test_run.py
@@ -5,22 +5,31 @@ from Model.model import AZero
 from Model.config import Config
 
 
-def timing(f):
-    def wrap(*args):
+def timing(func):
+    """ wrapper for timing functions
+    usage:
+    @timing
+    def time_test():
+        myfunc()
+    time_test()
+    """
+    def wrap(*args, **kwargs):
         time1 = time.time()
-        ret = f(*args)
-        t = time.time() - time1
-        h = int(t / 3600)
-        m = int((t % 3600) / 60)
-        s = int((t % 60))
-        if h is not 0:
-            print('{:s}() took {}h {}m {}s'.format(f.__name__, h, m, s))
-        elif m is not 0:
-            print('{:s}() took {}m {}s'.format(f.__name__, m, s))
-        elif s is not 0:
-            print('{:s}() took {:.2f}s'.format(f.__name__, t))
+        ret = func(*args, **kwargs)
+        total = time.time() - time1
+        hours = int(total / 3600)
+        minutes = int((total % 3600) / 60)
+        seconds = int((total % 60))
+        if hours is not 0:
+            print('{:s}() took {}h {}m {}s'.format(
+                func.__name__, hours, minutes, seconds))
+        elif minutes is not 0:
+            print('{:s}() took {}m {}s'.format(
+                func.__name__, minutes, seconds))
+        elif seconds is not 0:
+            print('{:s}() took {:.2f}s'.format(func.__name__, total))
         else:
-            print('{:s}() took {:.2f}ms'.format(f.__name__, t*1000))
+            print('{:s}() took {:.2f}ms'.format(func.__name__, total * 1000))
         return ret
     return wrap
 
@@ -29,9 +38,9 @@ if __name__ == "__main__":
     print("===== Executing Test Run =====")
     config = Config('Model/config.yaml')
     azero = AZero(config)
-    file = '_Data/training_data/dataset_685_games.pkl'
-    #file = '_Data/training_data/game_0000.pkl'
-    with open(file, 'rb') as f:
+    FILE = '_Data/training_data/dataset_685_games.pkl'
+    #FILE = '_Data/training_data/game_0000.pkl'
+    with open(FILE, 'rb') as f:
         train_data = pickle.load(f)
 
     @timing
