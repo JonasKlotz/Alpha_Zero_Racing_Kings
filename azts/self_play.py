8603302c1b8886bccff8cee133a1967327fd5568
Sebastian Zimmermann <seba@Sebastians-MacBook-Pro-3.fritz.box>
2020-05-24 12:06:09 +0200

    refactored self_play

--- azts/self_play.py
+++ azts/self_play.py
@@ -14,6 +14,21 @@ from config import *
 
 REPORT_CYCLE = 25 
 
+def unused_filename(i = 0):
+    filenumber = i
+
+    filenumberstring = str(filenumber).zfill(4)
+    filename = f"game_{filenumberstring}.pkl"
+    filepath = os.path.join(GAMEDIR, filename)
+    while os.path.isfile(filepath):
+        filenumber += 1
+        filenumberstring = str(filenumber).zfill(4)
+        filename = f"game_{filenumberstring}.pkl"
+        filepath = os.path.join(GAMEDIR, filename)
+
+    return filepath
+
+
 class SelfPlay():
     def __init__(self):
         self.match = self_match.SelfMatch(RUNS_PER_MOVE)
@@ -23,21 +38,13 @@ class SelfPlay():
             self.match.simulate()
             data = [tuple(j) for j in self.match.data_collection]
 
-            filenumber = i
-
-            filenumberstring = str(filenumber).zfill(4)
-            filename = f"game_{filenumberstring}.pkl"
-            filepath = os.path.join(GAMEDIR, filename)
-            while os.path.isfile(filepath):
-                filenumber += 1
-                filenumberstring = str(filenumber).zfill(4)
-                filename = f"game_{filenumberstring}.pkl"
-                filepath = os.path.join(GAMEDIR, filename)
+            filepath = unused_filename(i)
 
             pickle.dump(data, open(filepath, "wb"))
 
             del self.match
             self.match = self_match.SelfMatch(RUNS_PER_MOVE) 
+
 
 if __name__ == "__main__":
     play = SelfPlay()
