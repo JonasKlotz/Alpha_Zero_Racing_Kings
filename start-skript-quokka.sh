cedf51a6e3b8ec1c01efc0ffd79f9e7eb984456c
mkappen <m.kappen@campus.tu-berlin.de>
2020-06-16 20:30:24 +0200

    fix create_dataset. start skript v1.0

--- start-skript-quokka.sh
+++ start-skript-quokka.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-#SBATCH -o /../../../myjob.%j.%N.out   # Output-File
-#SBATCH -D /../../../                  # Working Directory
 #SBATCH -J AlphaZero_train_Gruppe1	# Job Name
-#SBATCH --nodes=2 		# Anzahl Knoten N
-#SBATCH --ntasks-per-node=20 	# Prozesse n pro Knoten
-#SBATCH --ntasks-per-core=1	# Prozesse n pro CPU-Core
-#SBATCH --mem=500M              # 500MiB resident memory pro node
+
+#SBATCH --nodes=8 		# Anzahl Knoten N
+#SBATCH --ntasks-per-node=1 	# Prozesse n pro Knoten
+#SBATCH --ntasks-per-core=1	  # Prozesse n pro CPU-Core
+#SBATCH --mem=10G              # 500MiB resident memory pro node
 
 ##Max Walltime vorgeben:
-#SBATCH --time=72:00:00 # Erwartete Laufzeit
+#SBATCH --time=00:05:00 # Erwartete Laufzeit
 
 #Auf Standard-Knoten rechnen:
 #SBATCH --partition=standard
 
+#SBATCH -o outfile_quokka                  # send stdout to outfile
+#SBATCH -e errfile_quokka                  # send stderr to errfile
+
 #Job-Status per Mail:
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=vorname.nachname@tu-berlin.de
+#SBATCH --mail-user=m.kappen@campus.tu-berlin.de
+
+ulimit -u 512
 
 module load nvidia/cuda/10.1
 module load nvidia/cudnn/7.6.5.32
@@ -25,4 +29,5 @@ module load nvidia/tensorrt/6.0.1.5
 cd
 source ~/env/bin/activate
 cd AlphaZero
-mpirun $myApplication
+mpirun python3 test_run.py
+deactivate
