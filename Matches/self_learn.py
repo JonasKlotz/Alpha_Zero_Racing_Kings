262243d606f8dc3ba7e34e5aa4e405a4017c407f
Thomas Gebauer <t.gebauer@campus.tu-berlin.de>
2020-07-16 14:20:48 +0200

    fixed del and added model SportySparrow

--- Matches/self_learn.py
+++ Matches/self_learn.py
@@ -61,8 +61,8 @@ def parallel_matches_with_preloaded_model(yamlpath, \
         i.join()
 
     # just to make sure: clean up
-    for i in selfplays:
-        del i
+    # for i in selfplays:
+    #     del i
 
 
 
